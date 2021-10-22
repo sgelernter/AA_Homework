2b5dc817b80fdbe85c9e97cf3c7bdd22f65adebb
sam <s.gelernter@gmail.com>
2021-10-21 23:43:08 -0400

    Complete map class

--- W3D4/diy_adts.rb
+++ W3D4/diy_adts.rb
@@ -41,3 +41,39 @@ class Queue
     end
 
 end
+
+class Map
+
+    def initialize
+        @map = []
+    end
+
+    def set(k, v)
+        all_keys = @map.map {|pair| pair.first }
+        if !all_keys.include?(k)
+            @map << [k, v]
+        else
+            @map.map! do |pair| 
+                if pair.first == k
+                    [k,v] 
+                else
+                    pair
+                end
+            end
+        end
+    end
+
+    def get(k)
+        @map.each do |pair|
+            return pair if pair.first == k 
+        end
+    end
+
+    def delete(k)
+        @map.delete_if { |pair| pair.first == k }
+    end
+
+    def show
+        print @map
+    end
+end
