2d06b8c2ce62647b7559351f82417d464dd52b11
sam <s.gelernter@gmail.com>
2021-10-21 23:29:19 -0400

    Complete stack class

--- W3D4/diy_adts.rb
+++ W3D4/diy_adts.rb
@@ -2,15 +2,22 @@ require "byebug"
 
 class Stack
 
+    attr_accessor :data
+
     def initialize
+        @data = []
     end
 
     def push(el)
+        data.push(el)
     end
 
     def pop
+        data.pop
     end
 
     def peek
+        data.last
     end
+    
 end
