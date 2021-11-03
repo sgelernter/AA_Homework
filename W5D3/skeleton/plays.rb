912e1f66f01352d5f5be5cab1d678c0d23895d32
sam <s.gelernter@gmail.com>
2021-11-03 01:07:30 -0400

    Complete Plays-Playwrights ORM

--- W5D3/skeleton/plays.rb
+++ W5D3/skeleton/plays.rb
@@ -73,3 +73,68 @@ class Play
    SQL
   end
 end
+
+class Playwright
+
+  attr_accessor :name, :birth_year, :id
+
+  def self.all
+    data = PlayDBConnection.instance.execute("SELECT * FROM playwrights")
+    data.map { |datum| Playwright.new(datum) }
+  end
+
+  def initialize(options)
+    @id = options['id']
+    @name = options['name']
+    @birth_year = options['birth_year']
+  end
+
+  def create
+    raise "#{self} already in database" if self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.name, self.birth_year)
+      INSERT INTO
+        playwrights(name, birth_year)
+      VALUES
+        (?, ?)
+    SQL
+    self.id = PlayDBConnection.instance.last_insert_row_id
+  end
+
+  def update
+    raise "#{self} not in database" unless self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.name, self.birth_year, self.id)
+      UPDATE
+        plays
+      SET
+        name = ?, birth_year = ?
+      WHERE
+        id = ?
+    SQL
+  end
+
+  def get_plays
+    PlayDBConnection.instance.execute(<<-SQL, self.name)
+      SELECT
+        *
+      FROM
+        plays
+        JOIN
+          playwrights ON playwrights.id = plays.playwright_id
+      WHERE
+        name = ?
+    SQL
+  end
+
+  def self.find_by_name(name)
+    PlayDBConnection.instance.execute(<<-SQL, name)
+      SELECT
+        *
+      FROM
+        playwrights
+      WHERE
+        name = ?
+    SQL
+  end
+
+
+end
