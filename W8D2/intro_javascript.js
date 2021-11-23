4e3b903a1319f47f573463531aef6e0e45e402a5
sam <s.gelernter@gmail.com>
2021-11-22 20:31:42 -0500

    Complete FizzBuzz

--- W8D2/intro_javascript.js
+++ W8D2/intro_javascript.js
@@ -3,3 +3,25 @@ function madLib(verb, adj, noun) {
 }
 
 madLib('eat', 'most', 'peppers');
+
+function isSubString(searchString, subString) {
+    var reg = new RegExp(subString)
+    if (reg.test(searchString)) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+}
+
+function fizzBuzz(array) {
+    let new_arr = []
+
+    array.forEach(numCheck)
+
+    function numCheck(num) {
+        if (num % 3 === 0 || num % 5 === 0) {
+            new_arr.push(num);
+        }
+    }
+    console.log(new_arr);
+}
